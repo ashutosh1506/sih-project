6014a835bbe77f1e424ad4e913f4a502a6726208
Ashutosh Gupta <guptaashutosh502@gmail.com>
2024-09-12 10:40:49 +0530

    Update PrivateRoute.jsx

--- front-end/src/Components/PrivateRoute.jsx
+++ front-end/src/Components/PrivateRoute.jsx
@@ -1,12 +1,18 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-
+import { useEffect } from "react";
 
 // This function checks if the user is logged in.
 const PrivateRoute = ({ element }) => {
-    const { isAuthenticated } = useAuth();
-  
-    return isAuthenticated ? element : <Navigate to="/login" />;
-  };
-  
-  export default PrivateRoute;
+  const { isAuthenticated } = useAuth();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      alert("Please login first");
+    }
+  }, [isAuthenticated]);
+
+  return isAuthenticated ? element : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
